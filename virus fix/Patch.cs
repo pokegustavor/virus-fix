edf92c3834b48405528d840b7fd3403691f57796
Gustavo Ribeiro <pokegustavo@gmail.com>
2021-03-30 23:20:28 -0300

    Adicionar arquivos de projeto.

---
+++ virus fix/Patch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace virus_fix
+{
+    [HarmonyPatch(typeof(PLShipInfo),"Update")]
+    public class Patch
+    {
+        static void Postfix(PLShipInfo __instance)
+        {
+            List<PLShipComponent> AllShipComponents = __instance.MyStats.AllComponents;
+            for (int i = AllShipComponents.Count - 1; i > -1; i--)
+            {
+                if (AllShipComponents[i].SlotType == ESlotType.E_COMP_CARGO && AllShipComponents[i].ActualSlotType == ESlotType.E_COMP_VIRUS)
+                {
+                    __instance.MyStats.RemoveShipComponent(AllShipComponents[i]);
+                }
+            }
+        }
+    }
+}
---
+++ virus fix/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using PulsarPluginLoader;
+
+namespace virus_fix
+{
+    public class Plugin : PulsarPlugin
+    {
+        public override string Version => "1.0";
+
+        public override string Author => "pokegustavo";
+
+        public override string ShortDescription => "Fixes virus in cargo glitch";
+
+        public override string Name => "Virus Fix";
+
+        public override string HarmonyIdentifier()
+        {
+            return "pokegustavo.Virus_Fix";
+        }
+    }
+}
